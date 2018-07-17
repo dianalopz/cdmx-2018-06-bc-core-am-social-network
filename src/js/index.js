02a0476d20eaf3efdb47f5fc7e88166a7029d820
dianalopz <dlauralr@icloud.com>
2018-07-17 18:50:35 -0500

    Agregando seccion de comentarios

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,11 @@ window.funciones={
 
 };
 module.exports=funciones;
+
+function myFunction () {
+  const comentario = document.getElementById("myTextarea").value;
+  const lugaraimprimir = document.getElementById("coments-card");
+  const nuevocoment = document.createElement('p');
+  nuevocoment.innerHTML = comentario;
+  lugaraimprimir.appendChild(nuevocoment);
+}
