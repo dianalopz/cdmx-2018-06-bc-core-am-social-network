18fc790acdb3d1446c596b7654ecd4f34cc4cc45
dianalopz <dlauralr@icloud.com>
2018-07-28 20:33:32 -0500

    Modificando funciones de logueo

--- src/js/data.js
+++ src/js/data.js
@@ -1,57 +1,80 @@
-(function() {
-  // Initialize Firebase
+window.petips = {
+  //Initialize Firebase
+  firebase: () => {
+    //Apis firebase
     var config = {
-      apiKey: "AIzaSyCf9mU0KwvA6Yz49EbBIu8LFKQqVVqZTxw",
-      authDomain: "petips-bb746.firebaseapp.com",
-      databaseURL: "https://petips-bb746.firebaseio.com",
-      projectId: "petips-bb746",
-      storageBucket: "petips-bb746.appspot.com",
-      messagingSenderId: "907345959081"
+      apiKey: 'AIzaSyCf9mU0KwvA6Yz49EbBIu8LFKQqVVqZTxw',
+      authDomain: 'petips-bb746.firebaseapp.com',
+      databaseURL: 'https://petips-bb746.firebaseio.com',
+      projectId: 'petips-bb746',
+      storageBucket: 'petips-bb746.appspot.com',
+      messagingSenderId: '907345959081'
     };
-
+    //Agregando apis a configuracion incial
     firebase.initializeApp(config);
+  },
 
-//Evento login con correo y contraseña
-btnLogin.addEventListener("click", e => {
-//Obtiene email and password
-const emailLogin = document.getElementById("txtEmailLogin").value;
-const passLogin = document.getElementById("txtPasswordLogin").value;
-//Pasa a la autenticacion de firebase
-firebase.auth().signInWithEmailAndPassword(emailLogin,passLogin)
-.then(result => {
-       location.href = ("views/Comentarios.html");
-     })
-.catch (e => alert(e.message));
-});
+  //Registro de nuevo usuario mail
+  newUser: (emailSignUp, passwordSignUp) => {
+    firebase.auth().createUserWithEmailAndPassword(emailSignUp, passwordSignUp)
+      .then(result => {
+        const user = firebase.auth().currentUser;
+        alert('Bienvenido a Petips');
+        location.href = ('views/Comentarios.html');
+      })
+      .catch (e => alert(e.message))
+  },
 
-//Evento login Google
-btnLoginGoogle.addEventListener("click", e => {
-//Pasa a la autenticacion de firebase primero validando si ya existe un usuario
-if (!firebase.auth().currentUser) {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  firebase.auth().signInWithPopup(provider).then(result => {
-    location.href = ("views/Comentarios.html");
-  })
-  .catch (e => alert(e.message));
-} else {
+  //Login con mail
+  loginWithEmail: (emailLogin, passLogin) => {
+    if(emailLogin != '' && passLogin != '') {
+      firebase.auth().signInWithEmailAndPassword(emailLogin, passLogin)
+        .then(result => {
+          location.href = ('views/Comentarios.html');
+        })
+        .catch (e => alert(e.message));
+    } else {
+      alert('Ingresa tu correo y contraseña para continuar');
+    }
+  },
+
+  //Login con Google
+  loginWithGoogle: () => {
+    //Pasa a la autenticacion de firebase primero validando si ya existe un usuario
+    if (!firebase.auth().currentUser) {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      firebase.auth().signInWithPopup(provider).then(result => {
+        location.href = ("views/Comentarios.html");
+      })
+      .catch (e => alert(e.message));
+    } else {
       firebase.auth().signOut();
     }
-});
+  },
 
-//Evento login con Facebook
-btnLoginFB.addEventListener("click", e => {
-//Pasa a la autenticacion de firebase primero validando si ya existe un usuario
-if (!firebase.auth().currentUser) {
-  const provider = new firebase.auth.FacebookAuthProvider();
-  provider.addScope('public_profile');
-  firebase.auth().signInWithPopup(provider).then(result => {
-    location.href = ("views/Comentarios.html");
-  })
-  .catch (e => alert(e.message));
-} else {
+  //Login con Facebook
+  loginWithFacebook: () => {
+    //Pasa a la autenticacion de firebase primero validando si ya existe un usuario
+    if (!firebase.auth().currentUser) {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      provider.addScope('public_profile');
+      firebase.auth().signInWithPopup(provider).then(result => {
+        location.href = ("views/Comentarios.html");
+      })
+      .catch (e => alert(e.message));
+    } else {
       firebase.auth().signOut();
     }
-});
+  },
 
-}());
+  //Cerrar sesión
+  signOut: () => {
+    firebase.auth().signOut()
+      .then(result => {
+        alert('Hasta pronto Petlover ♥')
+        location.href = ('../index.html');
+      })
+      .catch (e => alert(e.message));
+  }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,38 @@
-const InputNotEmpty=()=>{}
-module.exports=InputNotEmpty;
+//Agregando configuracion de firebase en js actual
+window.petips.firebase();
+
+//Evento registro nuevo usuario con correo y contraseña
+btnSignUp.addEventListener('click', e => {
+  const emailSignUp = document.getElementById('txtEmailSignUp').value;
+  const passwordSignUp = document.getElementById('txtPasswordSignUp').value;
+  //Envia parametros a funcion de registro
+  window.petips.newUser(emailSignUp, passwordSignUp);
+});
+
+//Evento login con correo y contraseña
+btnLogin.addEventListener('click', e => {
+  const emailLogin = document.getElementById('txtEmailLogin').value;
+  const passLogin = document.getElementById('txtPasswordLogin').value;
+  //Envia parametros a funcion de logeo
+  window.petips.loginWithEmail(emailLogin, passLogin);
+});
+
+//Evento login con Google
+btnLoginGoogle.addEventListener('click', e => {
+  window.petips.loginWithGoogle();
+});
+
+//Evento SignUp Google
+btnSignUpGoogle.addEventListener('click', e => {
+  window.petips.loginWithGoogle();
+});
+
+//Evento login con Facebook
+btnLoginFB.addEventListener('click', e => {
+  window.petips.loginWithFacebook();
+});
+
+//Evento SignUp Facebook
+btnSignUpFB.addEventListener('click', e => {
+  window.petips.loginWithFacebook();
+});
